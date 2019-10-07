f0e40992aa4cbcf2efbea8ff6f2cdd61163f591a
jctesta94 <jctesta94@gmail.com>
2019-10-06 21:01:40 -0400

    Data Wrangling 1

--- Main.R
+++ Main.R
@@ -1,15 +1,37 @@
+## This code is writen for practice use only
+
 library(tidyverse)
 library(rvest)
 
-main <- read_html('https://www.azlyrics.com/lyrics/hozier/movement.html')
+raw <- read_html('https://www.azlyrics.com/lyrics/tool.html')
 
+# ##
+# Scrape all artists from main page, store
+# loop through list and scrape all album and song pairings, store
+#   will store as accessable album song pair?
+# loop through list and pull lyrics for all songs, store
+# all words and letters will be parsed and tallied
+# create visuals for data
+# !Set limiter on this chunk before running, for good measure
+# ##
 
-
-rawST <- main %>% 
+rawST <- raw %>% 
   html_node(xpath = "//html//body//div[5]") %>%  #//div//div[2]//div[5]//text()[1]")
   html_text()
 
 rawST <- str_replace_all(rawST, "[\r\n]", " ")
 
 
+main <- as.data.frame(rawST) %>% 
+  mutate(rawST = strsplit(as.character(rawST), " ")) %>% 
+  unnest(rawST)
+  
+main$sorted <- main$rawST %>% 
+  na.omit() %>% 
+  #(-rawST) %>% 
+  str_remove_all("([(),])")
+  
+  
+  
+  
 rawST
